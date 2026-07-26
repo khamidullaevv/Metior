14b624ef3b70d43ece626b358d7516507644b6fd
khamidullaevv <yusufbek090110@gmail.com>
2026-07-26 22:20:35 +0500

    feat(fixing): fix import error on lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,30 @@
+use ashpd::desktop::screenshot::Screenshot;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+async fn capture_screenshot() -> Result<String, String> {
+    let response = Screenshot::request()
+        .interactive(true)
+        .modal(true)
+        .send()
+        .await
+        .map_err(|e| e.to_string())?
+        .response()
+        .map_err(|e| e.to_string())?;
+
+    Ok(response.uri().to_string())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, capture_screenshot])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
